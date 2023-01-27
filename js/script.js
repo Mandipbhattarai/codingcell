eef5e3e034a7c4ba107776267ff0b9d1890a6bbc
Mandipbhattarai <97532160+Mandipbhattarai@users.noreply.github.com>
2023-01-27 21:03:21 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -6,10 +6,3 @@ var typed = new Typed(".animate", {
     loop: true
 });
 
-// modal 
-var myModal = document.getElementById("myModal");
-var myInput = document.getElementById("myInput");
-
-myModal.addEventListener("shown.bs.modal", function() {
-    myInput.focus();
-});
